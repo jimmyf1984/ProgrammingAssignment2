e5caa8f1fbf599852bda692eac506305a6893dc1
jimmyf1984 <jimmyf1984@gmail.com>
2019-03-24 12:01:44 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -15,55 +15,35 @@
 # function, but it seems to be creating a variable "x" that is not reachable 
 # from the global environment, but is available in the environment of the 
 # makeCacheMatrix function
-makeCacheMatrix <- function(x = numeric()) {
-  
-  # holds the cached value or NULL if nothing is cached
-  # initially nothing is cached so set it to NULL
-  cache <- NULL
-  
-  # store a matrix
-  setMatrix <- function(newValue) {
-    x <<- newValue
-    # since the matrix is assigned a new value, flush the cache
-    cache <<- NULL
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
   }
-  
-  # returns the stored matrix
-  getMatrix <- function() {
-    x
-  }
-  
-  # cache the given argument 
-  cacheInverse <- function(solve) {
-    cache <<- solve
-  }
-  
-  # get the cached value
-  getInverse <- function() {
-    cache
-  }
-  
-  # return a list. Each named element of the list is a function
-  list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 # The following function calculates the inverse of a "special" matrix created with 
 # makeCacheMatrix
-cacheSolve <- function(y, ...) {
-  # get the cached value
-  inverse <- y$getInverse()
-  # if a cached value exists return it
-  if(!is.null(inverse)) {
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
     message("getting cached data")
-    return(inverse)
+    return(inv)
   }
-  # otherwise get the matrix, caclulate the inverse and store it in
-  # the cache
-  data <- y$getMatrix()
-  inverse <- solve(data)
-  y$cacheInverse(inverse)
-  
-  # return the inverse
-  inverse
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
